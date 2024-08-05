f27c3a586b89f3a599cc9c56991027c28596c25d
IshmamF <ishmamf2003@gmail.com>
2024-08-04 21:47:14 -0400

    tracks which active window you're on

--- main.go
+++ main.go
@@ -2,60 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"runtime"
-	"log"
-	"github.com/BurntSushi/xgb"
-	"github.com/BurntSushi/xgb/xproto"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(runtime.GOOS)
-	X, err := xgb.NewConn()
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Get the window id of the root window.
-	setup := xproto.Setup(X)
-	root := setup.DefaultScreen(X).Root
+	runtime.LockOSThread()
 
-	// Get the atom id (i.e., intern an atom) of "_NET_ACTIVE_WINDOW".
-	aname := "_NET_ACTIVE_WINDOW"
-	activeAtom, err := xproto.InternAtom(X, true, uint16(len(aname)),
-		aname).Reply()
-	if err != nil {
-		log.Fatal(err)
+	script := `
+    tell application "System Events"
+        set frontApp to name of first application process whose frontmost is true
+        tell process frontApp
+            get value of attribute "AXTitle" of window 1
+        end tell
+    end tell`
+	for {
+		cmd := exec.Command("osascript", "-e", script)
+		output, _ := cmd.CombinedOutput()
+		prettyOutput := strings.Replace(string(output), "\n", "", -1)
+		fmt.Println(prettyOutput)
+		time.Sleep(2 * time.Second)
 	}
-
-	// Get the atom id (i.e., intern an atom) of "_NET_WM_NAME".
-	aname = "_NET_WM_NAME"
-	nameAtom, err := xproto.InternAtom(X, true, uint16(len(aname)),
-		aname).Reply()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Get the actual value of _NET_ACTIVE_WINDOW.
-	// Note that 'reply.Value' is just a slice of bytes, so we use an
-	// XGB helper function, 'Get32', to pull an unsigned 32-bit integer out
-	// of the byte slice. We then convert it to an X resource id so it can
-	// be used to get the name of the window in the next GetProperty request.
-	reply, err := xproto.GetProperty(X, false, root, activeAtom.Atom,
-		xproto.GetPropertyTypeAny, 0, (1<<32)-1).Reply()
-	if err != nil {
-		log.Fatal(err)
-	}
-	windowId := xproto.Window(xgb.Get32(reply.Value))
-	fmt.Printf("Active window id: %X\n", windowId)
-
-	// Now get the value of _NET_WM_NAME for the active window.
-	// Note that this time, we simply convert the resulting byte slice,
-	// reply.Value, to a string.
-	reply, err = xproto.GetProperty(X, false, windowId, nameAtom.Atom,
-		xproto.GetPropertyTypeAny, 0, (1<<32)-1).Reply()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Active window name: %s\n", string(reply.Value))
 }
-
