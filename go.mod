8104434957747ec32e016b819fa9d51450360574
IshmamF <ishmamf2003@gmail.com>
2024-08-08 11:48:13 -0400

    sqlite implementation with create and read operations

--- go.mod
+++ go.mod
@@ -5,11 +5,20 @@ go 1.22.3
 require golang.org/x/sys v0.23.0
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgtype v1.14.3 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
-	golang.org/x/crypto v0.20.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	gorm.io/datatypes v1.2.1 // indirect
+	gorm.io/driver/mysql v1.5.6 // indirect
+	gorm.io/driver/sqlite v1.5.6 // indirect
+	gorm.io/gorm v1.25.11 // indirect
 )
--- main.go
+++ main.go
@@ -3,56 +3,15 @@ package main
 import (
 	"fmt"
 	// "github.com/IshmamF/productivity-monitor/window" Uncomment when using module
-	"context"
-	"log"
 	"time"
 
 	"github.com/IshmamF/productivity-monitor/darwin"
 	"github.com/IshmamF/productivity-monitor/utils"
-	"github.com/jackc/pgtype"
-	"github.com/jackc/pgx/v5"
+	//"gorm.io/datatypes"
 )
-
 
 func main() {
 	os := utils.Get_OS()
-	urlExample := "postgres://local:ishmam@localhost:5432/productivity"
-	conn, err := pgx.Connect(context.Background(), urlExample)
-	if err != nil {
-		fmt.Println("Unable to connect to database: \n", err)
-	} else {
-		fmt.Println("Database connected!")
-	}
-	defer conn.Close(context.Background())
-
-	type session_row struct {
-		session_id int
-		date pgtype.Date
-		start_time pgtype.Timestamp
-		end_time pgtype.Timestamp
-	}
-
-	rows, err := conn.Query(context.Background(), "SELECT * FROM session")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	var rowSlice []session_row
-	for rows.Next() {
-		var r session_row
-		err := rows.Scan(&r.session_id, &r.date, &r.start_time, &r.end_time)
-		if err != nil {
-			log.Fatal(err)
-		}
-	rowSlice = append(rowSlice, r)
-	}
-	if err := rows.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(rowSlice)
-
 
   	for {
 		var currWindow string
