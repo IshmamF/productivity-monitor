b5f24ee785a388896848a6b8a067bdb0dc375b65
IshmamF <ishmamf2003@gmail.com>
2024-08-09 19:09:06 -0400

    fixed apple script to see website url if on browser and combined with previous script to see application

--- darwin/current-window.go
+++ darwin/current-window.go
@@ -6,6 +6,7 @@ import (
 )
 
 func GetForegroundWindowData() string {
+	/*
 	script := `
 	global frontApp, appTitle, windowTitle
 	set windowTitle to "" 
@@ -16,8 +17,43 @@ func GetForegroundWindowData() string {
 			set windowTitle to value of attribute "AXTitle" of window 1
 		end tell
 	end tell
-	return {appTitle, windowTitle}`
-	cmd := exec.Command("osascript", "-e", script)
+	return {appTitle, windowTitle}`*/
+
+	test_script := `
+	tell application "System Events" to set frontApp to name of first application process whose frontmost is true
+	tell application "System Events" to set appTitle to title of first application process whose frontmost is true
+
+	global currentTabUrl, currentTabTitle, windowTitle
+	set currentTabUrl to ""
+	set currentTabTitle to ""
+	set windowTitle to ""
+
+	if (frontApp = "Safari") or (frontApp = "Webkit") then
+	  using terms from application "Safari"
+		tell application frontApp to set currentTabUrl to URL of front document
+		tell application frontApp to set currentTabTitle to name of front document
+	  end using terms from
+	else if (frontApp = "Google Chrome") or (frontApp = "Google Chrome Canary") or (frontApp = "Chromium") then
+	  using terms from application "Google Chrome"
+		tell application frontApp to set currentTabUrl to URL of active tab of front window
+		tell application frontApp to set currentTabTitle to title of active tab of front window
+	  end using terms from
+	else if (frontApp = "Arc") then
+	  using terms from application "Arc"
+		tell application frontApp to set currentTabUrl to URL of active tab of front window
+		tell application frontApp to set currentTabTitle to title of active tab of front window
+	  end using terms from
+	else
+		tell application "System Events"
+			tell process frontApp
+				set windowTitle to value of attribute "AXTitle" of window 1
+			end tell
+		end tell
+	end if
+	
+	return {currentTabUrl, currentTabTitle, appTitle, windowTitle}
+	`
+	cmd := exec.Command("osascript", "-e", test_script)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return "No window detected"
